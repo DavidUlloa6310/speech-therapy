9d6e0491f9f752cb6a3a9e318f5844b1426c6965
Jose Pujol <josepujol21@gmail.com>
2024-06-22 16:42:10 -0700

    Added CORS management

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/feedback")
+async def receive_feedback(file: UploadFile = File(...)):
+    return {"filename": file.filename, "content_type": file.content_type}
+
+    # if file.content_type != "audio/flac":
+    #         return JSONResponse(status_code=400, content={"message": "Invalid file type. Only FLAC files are accepted."})
+
+    # # Process the file (e.g., save it, analyze it, etc.)
+    # # For demonstration, we'll just read the content
+    # content = await file.read()
+    
+    # with open("received_audio.flac", "wb") as f:
+    #     f.write(content)
+
+    # return {"message": "File received successfully", "filename": file.filename}
